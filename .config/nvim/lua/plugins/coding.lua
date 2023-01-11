b8eb733b33267cd7a64d92cc53fc5cb4d5b4e9f9
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2023-01-11 10:24:06 +0100

    updated config

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -49,6 +49,7 @@ return {
           end,
         },
         mapping = cmp.mapping.preset.insert({
+          ['<TAB>'] = cmp.mapping(cmp.mapping.select_next_item()),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
