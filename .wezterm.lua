4c2a4c1476d3b438c3841ff6041d2316ca40e959
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2023-11-02 10:01:00 +0100

    stuff

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 config.font = wezterm.font("FiraCode Nerd Font")
 config.enable_scroll_bar = true
@@ -10,16 +10,16 @@ config.disable_default_key_bindings = false
 config.color_scheme = "Catppuccin Mocha (Gogh)"
 
 config.inactive_pane_hsb = {
-	hue = 1.0,
-	saturation = 1.0,
-	brightness = 1.0,
+    hue = 1.0,
+    saturation = 1.0,
+    brightness = 1.0
 }
 config.launch_menu = {}
 
 leader = {
-	key = "b",
-	mods = "CMD",
-	timeout_milliseconds = 2000,
+    key = "b",
+    mods = "CMD",
+    timeout_milliseconds = 2000
 }
 
 return config
