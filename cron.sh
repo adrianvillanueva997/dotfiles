56b491440bfea2abcd561c34bede562d0257a1a2
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2024-01-30 19:20:56 +0100

    removed stuf

--- cron.sh
+++
@@ -1,8 +0,0 @@
-# m h  dom mon dow   command
-@reboot root /usr/sbin/ntpd -n
-# reinicio semanal
-* 6 * * 0 /sbin/shutdown -r now
-# reinicio puntual
-# 00 6 * * * /usr/bin/bash reboot.sh
-# Borrado de docker
-0 3 * * * /usr/bin/docker system prune -f
