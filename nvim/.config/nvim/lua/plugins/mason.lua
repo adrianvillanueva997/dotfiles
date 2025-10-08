3ca2a7f5af817609082bea3a5d381c8dfb101b48
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2025-10-08 12:38:57 +0900

    update mason

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,20 +1,7 @@
 return {
-	"williamboman/mason.nvim",
-	opts = {
-		ensure_installed = {
-			"stylua",
-			"shellcheck",
-			"shfmt",
-			"lua-language-server",
-			"pyright",
-			"rust-analyzer",
-			"ruff",
-			"ty",
-			"gopls",
-			"typescript-language-server",
-			"biome",
-			"golangci-lint",
-			"zls",
-		},
-	},
+    "mason-org/mason.nvim",
+    opts = {
+        ensure_installed = {"stylua", "shellcheck", "shfmt", "lua-language-server", "pyright", "rust-analyzer", "ruff",
+                            "ty", "gopls", "typescript-language-server", "biome", "golangci-lint", "zls"}
+    }
 }
