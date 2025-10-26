e37a311f37ff836731d0a6943cda96a7fd15002e
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2025-10-26 13:20:51 +0900

    catppuccin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,65 @@
-return {
-	-- add gruvbox
-	{ "rebelot/kanagawa.nvim" },
-
-	-- Configure LazyVim to load gruvbox
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "kanagawa",
-		},
-	},
-}
+return { -- Catppuccin theme
+{
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = { -- Configure background variants
+            light = "latte",
+            dark = "mocha"
+        },
+        transparent_background = false,
+        show_end_of_buffer = false,
+        term_colors = true,
+        dim_inactive = {
+            enabled = false,
+            shade = "dark",
+            percentage = 0.15
+        },
+        styles = {
+            comments = {"italic"},
+            conditionals = {"italic"},
+            loops = {},
+            functions = {},
+            keywords = {"bold"},
+            strings = {},
+            numbers = {},
+            booleans = {"bold"},
+            properties = {},
+            types = {"bold"}
+        },
+        integrations = {
+            treesitter = true,
+            native_lsp = {
+                enabled = true,
+                virtual_text = {
+                    errors = {"italic"},
+                    hints = {"italic"},
+                    warnings = {"italic"},
+                    information = {"italic"}
+                },
+                underlines = {
+                    errors = {"undercurl"},
+                    hints = {"undercurl"},
+                    warnings = {"undercurl"},
+                    information = {"undercurl"}
+                }
+            },
+            telescope = true,
+            nvimtree = true,
+            which_key = true,
+            cmp = true,
+            gitsigns = true,
+            indent_blankline = {
+                enabled = true,
+                colored_indent_levels = false
+            }
+        }
+    }
+}, -- Tell LazyVim to load catppuccin as the colorscheme
+{
+    "LazyVim/LazyVim",
+    opts = {
+        colorscheme = "catppuccin"
+    }
+}}
