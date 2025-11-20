06518ac5f068a68b2a07bf75b0f3cee0e8a62e55
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2025-11-20 18:20:51 +0900

    fin

--- nvim/.config/nvim/lua/avm/plugins/noice.lua
+++ nvim/.config/nvim/lua/avm/plugins/noice.lua
@@ -1,18 +1,15 @@
 return -- lazy.nvim
 {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        -- add any options here
+    },
+    dependencies = { -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim", -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-    },
+    "rcarriga/nvim-notify"},
     config = function()
         require("noice").setup({
             lsp = {
@@ -20,8 +17,8 @@ return -- lazy.nvim
                 override = {
                     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                     ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-                },
+                    ["cmp.entry.get_documentation"] = true -- requires hrsh7th/nvim-cmp
+                }
             },
             -- you can enable a preset for easier configuration
             presets = {
@@ -29,8 +26,8 @@ return -- lazy.nvim
                 command_palette = true, -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false, -- add a border to hover docs and signature help
-            },
+                lsp_doc_border = false -- add a border to hover docs and signature help
+            }
         })
-    end,
+    end
 }
--- nvim/.config/nvim/lua/avm/plugins/snacks.lua
+++ nvim/.config/nvim/lua/avm/plugins/snacks.lua
@@ -4,32 +4,118 @@ return {
     lazy = false,
     opts = {
         -- Enable the modules you want
-        bigfile = { enabled = true },
-        dashboard = { enabled = true },
-        indent = { enabled = true },
-        input = { enabled = true },
-        notifier = { enabled = false },
-        quickfile = { enabled = true },
-        scroll = { enabled = true },
-        statuscolumn = { enabled = true },
-        words = { enabled = true },
-        lazygit = { enabled = true },
+        bigfile = {
+            enabled = true
+        },
+        dashboard = {
+            enabled = true
+        },
+        indent = {
+            enabled = true
+        },
+        input = {
+            enabled = true
+        },
+        notifier = {
+            enabled = false
+        },
+        quickfile = {
+            enabled = true
+        },
+        scroll = {
+            enabled = true
+        },
+        statuscolumn = {
+            enabled = true
+        },
+        words = {
+            enabled = true
+        },
+        lazygit = {
+            enabled = true
+        }
     },
-    keys = {
-        { "<leader>z", function() Snacks.zen() end, desc = "Toggle Zen Mode" },
-        { "<leader>Z", function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
-        { "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
-        { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
-        { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
-        { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
-        { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
-        { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
-        { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
-        { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
-        { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (CWD)" },
-        { "]]", function() Snacks.words.jump(1, true) end, desc = "Next Reference", mode = { "n", "t" } },
-        { "[[", function() Snacks.words.jump(-1, true) end, desc = "Prev Reference", mode = { "n", "t" } },
-    },
+    keys = {{
+        "<leader>z",
+        function()
+            Snacks.zen()
+        end,
+        desc = "Toggle Zen Mode"
+    }, {
+        "<leader>Z",
+        function()
+            Snacks.zen.zoom()
+        end,
+        desc = "Toggle Zoom"
+    }, {
+        "<leader>.",
+        function()
+            Snacks.scratch()
+        end,
+        desc = "Toggle Scratch Buffer"
+    }, {
+        "<leader>S",
+        function()
+            Snacks.scratch.select()
+        end,
+        desc = "Select Scratch Buffer"
+    }, {
+        "<leader>bd",
+        function()
+            Snacks.bufdelete()
+        end,
+        desc = "Delete Buffer"
+    }, {
+        "<leader>cR",
+        function()
+            Snacks.rename.rename_file()
+        end,
+        desc = "Rename File"
+    }, {
+        "<leader>gB",
+        function()
+            Snacks.gitbrowse()
+        end,
+        desc = "Git Browse"
+    }, {
+        "<leader>gb",
+        function()
+            Snacks.git.blame_line()
+        end,
+        desc = "Git Blame Line"
+    }, {
+        "<leader>gf",
+        function()
+            Snacks.lazygit.log_file()
+        end,
+        desc = "Lazygit Current File History"
+    }, {
+        "<leader>gg",
+        function()
+            Snacks.lazygit()
+        end,
+        desc = "Lazygit"
+    }, {
+        "<leader>gl",
+        function()
+            Snacks.lazygit.log()
+        end,
+        desc = "Lazygit Log (CWD)"
+    }, {
+        "]]",
+        function()
+            Snacks.words.jump(1, true)
+        end,
+        desc = "Next Reference",
+        mode = {"n", "t"}
+    }, {
+        "[[",
+        function()
+            Snacks.words.jump(-1, true)
+        end,
+        desc = "Prev Reference",
+        mode = {"n", "t"}
+    }},
     init = function()
         vim.api.nvim_create_autocmd("User", {
             pattern = "VeryLazy",
@@ -42,7 +128,7 @@ return {
                     Snacks.debug.backtrace()
                 end
                 vim.print = _G.dd -- Override print to use snacks for better debugging
-            end,
+            end
         })
-    end,
+    end
 }
