3e2d9fd0443d8ed2b6c24934d14aed32f490f9bc
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2025-11-27 20:54:35 +0900

    fix python

--- nvim/.config/nvim/lua/avm/plugins/telescope.lua
+++ nvim/.config/nvim/lua/avm/plugins/telescope.lua
@@ -5,6 +5,31 @@ return {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make"
     }, "nvim-tree/nvim-web-devicons"},
+    keys = {{
+        "<leader>ff",
+        "<cmd>Telescope find_files<cr>",
+        desc = "Fuzzy find files in cwd"
+    }, {
+        "<leader>fr",
+        "<cmd>Telescope oldfiles<cr>",
+        desc = "Fuzzy find recent files"
+    }, {
+        "<leader>fs",
+        "<cmd>Telescope live_grep<cr>",
+        desc = "Find string in cwd"
+    }, {
+        "<leader>fc",
+        "<cmd>Telescope grep_string<cr>",
+        desc = "Find string under cursor in cwd"
+    }, {
+        "<leader>fb",
+        "<cmd>Telescope buffers<cr>",
+        desc = "Find buffers"
+    }, {
+        "<leader>fh",
+        "<cmd>Telescope help_tags<cr>",
+        desc = "Find help tags"
+    }},
     config = function()
         local telescope = require("telescope")
         local actions = require("telescope.actions")
@@ -16,33 +41,16 @@ return {
                     i = {
                         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
                         ["<C-j>"] = actions.move_selection_next, -- move to next result
-                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist
+                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                        ["<CR>"] = actions.select_tab -- Open in new tab on Enter
+                    },
+                    n = {
+                        ["<CR>"] = actions.select_tab -- Open in new tab on Enter
                     }
                 }
             }
         })
 
         telescope.load_extension("fzf")
-
-        local keymap = vim.keymap -- for conciseness
-
-        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {
-            desc = "Fuzzy find files in cwd"
-        })
-        keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", {
-            desc = "Fuzzy find recent files"
-        })
-        keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", {
-            desc = "Find string in cwd"
-        })
-        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", {
-            desc = "Find string under cursor in cwd"
-        })
-        keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {
-            desc = "Find buffers"
-        })
-        keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {
-            desc = "Find help tags"
-        })
     end
 }
--- nvim/.config/nvim/lua/avm/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/avm/plugins/treesitter.lua
@@ -1,69 +1,30 @@
 return {{
     "nvim-treesitter/nvim-treesitter",
     event = {"BufReadPre", "BufNewFile"},
-    cmd = {"TSUpdate", "TSInstall", "TSLog", "TSUninstall"},
-    opts_extend = {"ensure_installed"},
     build = ":TSUpdate",
-    config = function()
-        -- import nvim-treesitter plugin
-        local treesitter = require("nvim-treesitter.configs")
-        -- configure treesitter
-        treesitter.setup({ -- enable syntax highlighting
-            highlight = {
-                enable = true,
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                    if lang == "python" then
-                        return true
-                    end
-                end
-            },
-            folds = {
-                enable = true
-            },
-            -- enable indentation
-            indent = {
-                enable = true
-            },
-            -- ensure these languages parsers are installed
-            ensure_installed = {"bash", "html", "javascript", "json", "lua", "markdown", "markdown_inline", "python",
-                                "regex", "rust", "tsx", "typescript", "vim", "yaml", "zig"},
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = false
-                }
-            },
-            additional_vim_regex_highlighting = false
-        })
-    end
-}, -- NOTE: js,ts,jsx,tsx Auto Close Tags
-{
-    "windwp/nvim-ts-autotag",
-    enabled = true,
-    ft = {"html", "xml", "javascript", "typescript", "javascriptreact", "typescriptreact", "svelte"},
-    config = function()
-        -- Independent nvim-ts-autotag setup
-        require("nvim-ts-autotag").setup({
-            opts = {
-                enable_close = true, -- Auto-close tags
-                enable_rename = true, -- Auto-rename pairs
-                enable_close_on_slash = false -- Disable auto-close on trailing `</`
-            },
-            per_filetype = {
-                ["html"] = {
-                    enable_close = true -- Disable auto-closing for HTML
-                },
-                ["typescriptreact"] = {
-                    enable_close = true -- Explicitly enable auto-closing (optional, defaults to `true`)
-                }
+    main = "nvim-treesitter.configs",
+    opts = {
+        ensure_installed = {"bash", "html", "javascript", "json", "lua", "markdown", "markdown_inline",
+                            "regex", "rust", "tsx", "typescript", "vim", "yaml", "zig"},
+        highlight = {
+            enable = true,
+            disable = {"python"} -- Disabled due to parser issues
+        },
+        indent = {
+            enable = true
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-space>",
+                node_incremental = "<C-space>",
+                scope_incremental = false,
+                node_decremental = "<bs>"
             }
-        })
-    end
+        }
+    }
+}, {
+    "windwp/nvim-ts-autotag",
+    event = {"BufReadPre", "BufNewFile"},
+    opts = {}
 }}
